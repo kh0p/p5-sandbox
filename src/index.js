0e6fc7f1a74b755a138a47d72712a23e2c275d0f
楊文里 <kh0p@users.noreply.github.com>
2016-07-06 20:42:36 +0200

    :rat: Cleanup of index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-var canvas = {
-  w: 0,
-  h: 0
-};
-
 function setup() {
-  canvas.w = 730;
-  canvas.h = 697;
-
-  createCanvas(canvas.w, canvas.h);
+  createCanvas(640, 480);
   background(0);
-
-  var color   = {
-    r: 0,
-    g: 0,
-    b: 0
-  };
 }
 
 function draw() {
-  var rWidth  = random(0, canvas.w);
-  var rHeight = random(0, canvas.h);
-  color.r = random(80, 180);
-  color.g = random(0, 10);
-  color.b = random(100, 200);
-  var d   = random(6, 16);
-
-  noStroke();
-  fill(color.r, color.g, color.b, 100)
-  ellipse(rWidth, rHeight, d, d);
 }
